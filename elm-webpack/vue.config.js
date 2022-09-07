695cc98a92251e6a6ece6798e2cf9a99c8a4af93
Rhodri-hc <13622450494@163.com>
2022-09-07 12:16:43 +0800

    webpack 构建优化

--- elm-webpack/vue.config.js
+++ elm-webpack/vue.config.js
@@ -2,7 +2,15 @@ const path = require('path');
 const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const AddAssetHtmlWebpackPlugin = require("add-asset-html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+const AddAssetHtmlWebpackPlugin = require("add-asset-html-webpack-plugin")
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin')
+
+const PATHS = {
+  src: path.join(__dirname, 'src')
+}
 
 const smp = new SpeedMeasureWebpackPlugin({
   disable: !(process.env.MEASURE === 'true'),
@@ -11,11 +19,65 @@ const smp = new SpeedMeasureWebpackPlugin({
 
 // console.log("require('os').cpus()", require('os').cpus());
 
+const dllReference = (config) => {
+  config.plugin('vendorDll')
+    .use(webpack.DllReferencePlugin, [{
+      context: __dirname,
+      manifest: require('./dll/vue-manifest.json')
+    }]);
+  // 这里是把相关文件引用入到html模板中
+  config.plugin('addAssetHtml')
+    .use(AddAssetHtmlWebpackPlugin, [
+      [
+        {
+          filepath: require.resolve(path.resolve(__dirname, './dll/vue.dll.js')),
+        }
+      ]
+    ])
+    .after('html')
+};
+
 module.exports = {
-  // publicPath: "./",
+  publicPath: "./",
   // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
   parallel: true,
   configureWebpack: smp.wrap({
+    cache: {
+      type: 'filesystem',
+      cacheDirectory: path.resolve(__dirname, './node_modules/.temp_cache'),
+    },
+    module: {
+      rules:[
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use:[
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                // optipng.enabled: false will disable optipng
+                optipng: {
+                  enabled: false,
+                },
+                pngquant: {
+                  quality: [0.65, 0.90],
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false,
+                },
+                // the webp option will enable WEBP
+                webp: {
+                  quality: 75
+                }
+              }
+            }
+          ]
+        }
+      ]
+    },
     resolve: {
         alias: {
             'src': path.resolve(__dirname, './src'),
@@ -45,18 +107,35 @@ module.exports = {
         // analyzerHost:""
         // analyzerPort
       }),
-      new webpack.DllReferencePlugin({
-        context: __dirname,
-        manifest: path.resolve(__dirname, './dll/vue-manifest.json')
+      new PurgeCSSPlugin({
+        paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
       }),
-      new AddAssetHtmlWebpackPlugin({
-        filepath: path.resolve(__dirname, './dll/vue.dll.js'),
-        publicPath: './',
-        outputPath: path.resolve(__dirname, './dist'),
-        // attributes: {
-        //   nomodule: false
-        // },
-      })
+      // new webpack.DllReferencePlugin({
+      //   context: path.join(__dirname),
+      //   manifest: path.resolve(__dirname, './dll/vue-manifest.json')
+      // }),
+      // // new CopyWebpackPlugin({
+      // //   patterns:[
+      // //     { from: path.resolve(__dirname, "./dll/vue.dll.js"),
+      // //       to: path.resolve(__dirname, "./dist/js/vue.dll.js")
+      // //     }
+      // //   ]
+      // // }),
+      // // new HtmlWebpackTagsPlugin({
+      // //   scripts: "/js/vue.dll.js",
+      // //   append: true
+      // // })
+      // new AddAssetHtmlWebpackPlugin({
+      //   filepath: path.resolve(__dirname, './dll/vue.dll.js'),
+      //   publicPath: './dll',
+      //   outputPath: './dist',
+      // })
     ]
-  })
+  }),
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      dllReference(config)
+    }
+  }
+
 }
